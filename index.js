802c545c7e234c89310c46f60f31f0cefd037bc1
Taras Mitran <taras.mitran@payscale.com>
2019-03-20 15:20:26 -0400

    allow multiple db pools

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const _ = require('lodash');
 const Cursor = require('pg-cursor');
 const parseConnectionString = require('./lib/parseConnectionString');
 const listenNotifyClient = require('./lib/listenNotifyClient');
+const pgPools = {};
 
 /*
   See readme for an overview
  */
-module.exports = function(pg, connString) {
-  
-  const config = exports.pgConfig = parseConnectionString(connString);
+module.exports = function (pg, configInput) {
 
-  const pool = exports.pgPool = new pg.Pool(config);
+  let config = configInput;
+  if (configInput.pg_conn_string && !configInput.database) {
+    config = _.merge(config, parseConnectionString(config.pg_conn_string));
+  }
+
+  const connIdentifier = createConnIdentifier(config);
+
+  const pool = new pg.Pool(config);
+  pgPools[connIdentifier] = pool;
 
   /*
     global logging -------------------------------
    */
-  pool.on('error', function(error, client) {
+  pool.on('error', function (error, client) {
     try {
       let lastQuery;
       if (client) {
@@ -26,7 +33,7 @@ module.exports = function(pg, connString) {
       }
       console.error('pg-pool error after last query: ', lastQuery);
       console.error('pg-pool error: ', error);
-    } catch(e) {
+    } catch (e) {
       console.error('Could not log a postgres error due to circular json');
     }
   });
@@ -34,10 +41,10 @@ module.exports = function(pg, connString) {
   // ---------------------------------------------
 
   // attaching method to pg.Client so all the APIs that use a singleton pgClient.queryAsync will use connection pooling
-  pg.Client.prototype.queryAsync = Promise.promisify(function(query, bindVars, queryCB) {
+  pg.Client.prototype.queryAsync = Promise.promisify(function (query, bindVars, queryCB) {
 
     let client = this;
-
+    const connName = createConnIdentifier((client.connectionParameters));
     // if no bind vars
     if (queryCB == undefined) {
       queryCB = bindVars;
@@ -45,52 +52,61 @@ module.exports = function(pg, connString) {
     }
 
     if (this.inTransaction) {
-     client.lastQuery = { query, bindVars }; 
-     client.query(query, bindVars, queryCB);
+      client.lastQuery = {query, bindVars};
+      client.query(query, bindVars, queryCB);
     } else {
-      pool.connect(function(connectErr, pgClient, connectFinishFn) {
+      pgPools[connName].connect(function (connectErr, pgClient, connectFinishFn) {
         if (connectErr) {
           console.error('database connection error: ', connectErr);
           connectFinishFn(connectErr);
         } else {
-          pgClient.lastQuery = { query, bindVars }; 
-          pgClient.query(query, bindVars, function(err, queryRes) {
+          pgClient.connName = client.connName;
+          pgClient.lastQuery = {query, bindVars};
+          pgClient.query(query, bindVars, function (err, queryRes) {
             connectFinishFn();
             queryCB(err, queryRes);
           });
-        }  
+        }
       });
-    }   
+    }
   });
 
   // grab client from pool, then create begin transaction
-  pg.Client.prototype.transactionStart = Promise.promisify(function(cb) {
-    pool.connect(function(connectErr, pgClient, connectFinishFn) {
-      pgClient.returnClientToPool = connectFinishFn;
-      pgClient.inTransaction = true;
-      pgClient.query('BEGIN', function(err, beginResult) {
-        cb(connectErr, pgClient);
-      });
+  pg.Client.prototype.transactionStart = Promise.promisify(function (cb) {
+    let client = this;
+    const connName = createConnIdentifier((client.connectionParameters));
+
+    pgPools[connName].connect(function (connectErr, pgClient, connectFinishFn) {
+      if (!connectErr) {
+        pgClient.connName = client.connName;
+        pgClient.returnClientToPool = connectFinishFn;
+        pgClient.inTransaction = true;
+        pgClient.query('BEGIN', function (err, beginResult) {
+          cb(connectErr, pgClient);
+        });
+      } else {
+        console.error('transactionStart during pool.connect', connectErr);
+      }
     });
   });
- 
+
   // commit + return client to connection pool
-  pg.Client.prototype.commit = Promise.promisify(function(cb) {
+  pg.Client.prototype.commit = Promise.promisify(function (cb) {
     let client = this;
     client.inTransaction = false;
-    client.query('COMMIT', function(err, commitResult) {
+    client.query('COMMIT', function (err, commitResult) {
       client.returnClientToPool();
-      cb(err, commitResult); 
+      cb(err, commitResult);
     });
   });
 
   // rollback + return client to connection pool
-  pg.Client.prototype.rollback = Promise.promisify(function(cb) {
+  pg.Client.prototype.rollback = Promise.promisify(function (cb) {
     let client = this;
     client.inTransaction = false;
-    client.query('ROLLBACK', function(err, rollbackResult) {
+    client.query('ROLLBACK', function (err, rollbackResult) {
       client.returnClientToPool();
-      cb(err, rollbackResult); 
+      cb(err, rollbackResult);
     });
   });
 
@@ -98,7 +114,7 @@ module.exports = function(pg, connString) {
    *   since template strings do not allow templates-within-templates
    *   and a shortcut way to just inject blocks of sql that is trusted (i.e. not input from ajax call)
    */
-  const convertHandlebarsTemplateToQuery = function(obj, substVals) {
+  const convertHandlebarsTemplateToQuery = function (obj, substVals) {
     if (substVals) {
       let numSubstTries = 0;
       while (numSubstTries < 5 && obj.query.match(/{{/)) {
@@ -107,22 +123,22 @@ module.exports = function(pg, connString) {
         obj.query = compiled(substVals);
         numSubstTries++;
       }
-    }  
+    }
   };
 
   // adds the main method to the pg client class for querying data in this simplified bind variables design
-  pg.Client.prototype.queryTmpl = function(obj, substVals) {
-  
+  pg.Client.prototype.queryTmpl = function (obj, substVals) {
+
     convertHandlebarsTemplateToQuery(obj, substVals);
 
     let me = this;
-    let queryWrapper = new Promise(function(resolve, reject) {
+    let queryWrapper = new Promise(function (resolve, reject) {
       pg.Client.prototype.queryAsync.call(me, obj.query, obj.values)
-        .then(function(queryResult) {
+        .then(function (queryResult) {
           queryResult = _.pick(queryResult, ['rowCount', 'rows']);
           resolve(queryResult);
         })
-        .catch(function(e) {
+        .catch(function (e) {
           console.error('query error in promise', e);
           reject(e);
         });
@@ -138,52 +154,56 @@ module.exports = function(pg, connString) {
       and remove statement timeout settings for cursors since they meant to be are read-only long running
    */
 
-  const cursorQuery = function(query, bindVars, cursorCreateCB) {
- 
-    pool.connect(function(connectErr, pgClient, connectFinishFn) {
-      pgClient.query('BEGIN', function(transactionBeginErr) {
-        if(transactionBeginErr) {
+  const cursorQuery = function (query, bindVars, cursorCreateCB) {
+
+    let client = this;
+    const connName = createConnIdentifier((client.connectionParameters));
+
+    pgPools[connName].connect(function (connectErr, pgClient, connectFinishFn) {
+      pgClient.connName = client.connName;
+      pgClient.query('BEGIN', function (transactionBeginErr) {
+        if (transactionBeginErr) {
           console.error('query error begining transaction', transactionBeginErr);
           return connectFinishFn(err);
         }
 
         // cursors are meant to stream bulk data so no timeout
-        pgClient.query('SET statement_timeout = 0', function(timeoutErr, setTimeoutRes) {
-          if(timeoutErr) {
+        pgClient.query('SET statement_timeout = 0', function (timeoutErr, setTimeoutRes) {
+          if (timeoutErr) {
             console.error('error setting the postgres statement_timeout setting', timeoutErr);
-            pgClient.query('ROLLBACK', function(rollbackErr, rollbackResult) {
+            pgClient.query('ROLLBACK', function (rollbackErr, rollbackResult) {
               console.error('rolling back transaction', rollbackErr);
               return connectFinishFn(err);
-            });  
+            });
           }
 
           let cursor = pgClient.query(new Cursor(query, bindVars));
           cursor.endConnection = connectFinishFn;
 
-          cursor.readAsync = Promise.promisify(function(numRows, readWrapperCB) {
-        
-            cursor.read(numRows, function(cursorReadErr, rows) {
+          cursor.readAsync = Promise.promisify(function (numRows, readWrapperCB) {
+
+            cursor.read(numRows, function (cursorReadErr, rows) {
               if (cursorReadErr) {
                 console.error('cursor read error', cursorReadErr);
-                pgClient.query('ROLLBACK', function(rollbackErr, rollbackResult) {
+                pgClient.query('ROLLBACK', function (rollbackErr, rollbackResult) {
                   console.error('rollback after cursor read error done');
                   connectFinishFn(rollbackErr);
                   return readWrapperCB(rollbackErr);
                 });
               } else if (!rows.length) {
-                pgClient.query('COMMIT', function(commitErr) {
+                pgClient.query('COMMIT', function (commitErr) {
                   if (commitErr) {
                     console.error('error committing cursor transaction', commitErr);
-                  }  
+                  }
                   connectFinishFn();
                 });
                 return readWrapperCB(null, rows);
               } else {
-                readWrapperCB(null, rows); 
+                readWrapperCB(null, rows);
               }
             });
-          }); 
-     
+          });
+
           cursorCreateCB(connectErr, cursor);
         });
       });
@@ -193,7 +213,7 @@ module.exports = function(pg, connString) {
   pg.Client.prototype.cursorAsync = Promise.promisify(cursorQuery);
 
   // parallel wrapper of queryTmpl but for cursors
-  pg.Client.prototype.cursorTmpl = Promise.promisify(function(obj, substVals, cursorCB) {
+  pg.Client.prototype.cursorTmpl = Promise.promisify(function (obj, substVals, cursorCB) {
 
     // if no bind vars
     if (cursorCB == undefined) {
@@ -208,7 +228,7 @@ module.exports = function(pg, connString) {
 
   // convert @param pieces (arguments to sqlTemplate) to bind variable strings
   //   such as $1 and ['foo']
-  exports.sqlTemplate = pg.Client.prototype.sqlTmpl = function(pieces) {
+  const sqlTemplate = pg.Client.prototype.sqlTmpl = function (pieces) {
     let result = '';
     let vals = [];
     let substitutions = [].slice.call(arguments, 1);
@@ -223,11 +243,14 @@ module.exports = function(pg, connString) {
   };
 
   // wrapper for lib to share the existing connection string for general postgres pool
-  pg.Client.prototype.listenNotify = function(channelName, callback) {
-    listenNotifyClient(connString, channelName, callback);
+  pg.Client.prototype.listenNotify = function (channelName, callback) {
+    listenNotifyClient(config.pg_conn_string, channelName, callback);
     return {success: true}
-  } 
+  };
 
-  return exports;
+  return { pool, config, pgClient: pg.Client, sqlTemplate };
 };
 
+function createConnIdentifier(config) {
+  return `${config.user}${config.host}${config.port ? config.port : 5432}${config.database}`;
+}
